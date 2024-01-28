fc742a9cf8176bb9dae7b79922139625b1e3563d
m3mo <mehmetozdagg@gmail.com>
2024-01-29 00:33:00 +0100

    tester4

--- 1_HelloJava/tester.java
+++ 1_HelloJava/tester.java
@@ -3,10 +3,5 @@ public class tester {
     public static void main (String[] args) {
 
         System.out.println("Tester");
-        System.out.println("Tester2");
-        System.out.println("Tester3");
-
-
-
     }
 }
