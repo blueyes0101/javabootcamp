d228400999cd26f2bbdf274a1f1abc5f457b8865
m3mo <mehmetozdagg@gmail.com>
2024-01-29 02:15:22 +0100

    #

---
+++ j5CharBoolean/CharBoolean.java
@@ -0,0 +1,17 @@
+package j5CharBoolean;
+
+public class CharBoolean {
+    public static void main (String [] args){
+        //Char = 2 byte - 2^16 
+        //Boolean = true/false
+
+        char a = 'A';
+        char b = '?';
+        char c = '\u2121'; // unicodetable
+        System.out.println(c);
+
+        boolean d = true;
+        System.out.println(d);
+
+    }
+}
