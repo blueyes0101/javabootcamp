73ea04a76aa12bd8a477725d69907b1393cd1d73
m3mo <mehmetozdagg@gmail.com>
2024-01-29 01:18:34 +0100

    #

---
+++ j2Variable/variable.java
@@ -0,0 +1,12 @@
+package j2Variable;
+public class variable {
+    public static void main (String[] args){
+        int a = 4;
+        int b = a + 5;
+        int c = b - 2;
+        int summe = a + b * c;
+        System.out.println("Summe= " + summe);
+        //Summe= 67
+    }
+    
+}
